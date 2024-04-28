7a7078c0c106e9d8b7897012da1ca1e9d5980e7e
Rav <84037256+rav-ctrl@users.noreply.github.com>
2024-04-28 00:23:18 -0400

    rm retrofit

--- app/src/main/java/com/example/unfilm/movies/data/remote/response/MovieListDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.unfilm.movies.data.remote.response
-
-data class MovieListDto(
-    val page: Int,
-    val movieDtos: List<MovieDto>,
-    val total_pages: Int,
-    val total_results: Int
-)
